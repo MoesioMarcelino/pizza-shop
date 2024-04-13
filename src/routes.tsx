5b79e478790abfda792fdc5c3e4697c37081ff32
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-13 12:17:48 -0300

    feat: add layouts to authenticated and unauthenticated pages

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,9 +2,19 @@ import { createBrowserRouter } from 'react-router-dom'
 
 import { Dashboard } from '@/pages/app/dashboard'
 
+import { AppLayout } from './pages/_layouts/app'
+import { AuthLayout } from './pages/_layouts/auth'
 import { SignIn } from './pages/auth/signIn'
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Dashboard /> },
-  { path: '/sign-in', element: <SignIn /> },
+  {
+    path: '/',
+    element: <AppLayout />,
+    children: [{ path: '/', element: <Dashboard /> }],
+  },
+  {
+    path: '/sign-in',
+    element: <AuthLayout />,
+    children: [{ path: '/sign-in', element: <SignIn /> }],
+  },
 ])

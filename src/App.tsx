2ea9d400a1c7281e23c9073fcc8cf583e78fa854
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-13 12:03:03 -0300

    feat: project setup (vite, eslint, tailwind, ts, shadcnUI)

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './global.css'
+
+import { Button } from './components/ui/button'
+
+export function App() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <Button>Enviar</Button>
+    </div>
+  )
+}

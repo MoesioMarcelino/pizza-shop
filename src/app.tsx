6bf926fe6d378b117b9cdafeb0a077baa8e19a93
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-13 17:58:02 -0300

    feat: add conection within backend and configure react-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,12 @@
 import './global.css'
 
+import { QueryClientProvider } from '@tanstack/react-query'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { ThemeProvider } from '@/components/theme/theme-provider'
+import { queryClient } from '@/lib/react-query'
 import { router } from '@/routes'
 
 export function App() {
@@ -13,7 +15,9 @@ export function App() {
       <Helmet titleTemplate="%s | Pizza.Shop" />
       <ThemeProvider defaultTheme="dark" storageKey="pizza-shop-theme">
         <Toaster richColors />
-        <RouterProvider router={router} />
+        <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+        </QueryClientProvider>
       </ThemeProvider>
     </HelmetProvider>
   )

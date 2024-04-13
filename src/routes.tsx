1949773b1bd4c9d05780463c3c168b6f42b455e7
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-13 12:11:22 -0300

    feat: add react-router-dom and setup basic routes to test them

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from 'react-router-dom'
+
+import { Dashboard } from '@/pages/app/dashboard'
+
+import { SignIn } from './pages/auth/signIn'
+
+export const router = createBrowserRouter([
+  { path: '/', element: <Dashboard /> },
+  { path: '/sign-in', element: <SignIn /> },
+])

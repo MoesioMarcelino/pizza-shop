d95a69bd668c474314e6ce0933b19066342e853f
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-13 15:17:55 -0300

    feat: add popular products chart and its customized labels

---
+++ src/pages/app/dashboard/popular-products-chart.tsx
@@ -0,0 +1,103 @@
+import { BarChart } from 'lucide-react'
+import { Cell, Pie, PieChart, ResponsiveContainer } from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+
+const data = [
+  { product: 'Pepperoni', amount: 40 },
+  { product: 'Mussarela', amount: 30 },
+  { product: 'Marguerita', amount: 50 },
+  { product: 'Portuguesa', amount: 16 },
+  { product: 'Calabresa', amount: 26 },
+]
+
+const COLORS = [
+  colors.sky[500],
+  colors.amber[500],
+  colors.violet[500],
+  colors.emerald[500],
+  colors.rose[500],
+]
+
+type CustomizedLabelProps = {
+  cx: number
+  cy: number
+  midAngle: number
+  innerRadius: number
+  outerRadius: number
+  value: number
+  index: number
+}
+
+function CustomizedLabel({
+  cx,
+  cy,
+  index,
+  innerRadius,
+  midAngle,
+  outerRadius,
+  value,
+}: CustomizedLabelProps) {
+  const RADIAN = Math.PI / 180
+  const radius = 12 + innerRadius + (outerRadius - innerRadius)
+  const x = cx + radius * Math.cos(-midAngle * RADIAN)
+  const y = cy + radius * Math.sin(-midAngle * RADIAN)
+
+  return (
+    <text
+      x={x}
+      y={y}
+      className="fill-muted-foreground text-xs"
+      textAnchor={x > cx ? 'start' : 'end'}
+      dominantBaseline="central"
+    >
+      {data[index].product.length > 12
+        ? data[index].product.substring(0, 12).concat('...')
+        : data[index].product}{' '}
+      ({value})
+    </text>
+  )
+}
+
+export function PopularProductsChart() {
+  return (
+    <Card className="col-span-3">
+      <CardHeader className="pb-8">
+        <div className="flex items-center justify-between">
+          <CardTitle className="text-base font-medium">
+            Produtos populares
+          </CardTitle>
+          <BarChart className="h-4 w-4 text-muted-foreground" />
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <PieChart style={{ fontSize: 12 }}>
+            <Pie
+              data={data}
+              dataKey="amount"
+              nameKey="product"
+              cx="50%"
+              cy="50%"
+              outerRadius={86}
+              innerRadius={64}
+              strokeWidth={8}
+              labelLine={false}
+              label={CustomizedLabel}
+            >
+              {data.map((_, index) => (
+                <Cell
+                  key={`cell-${index}`}
+                  fill={COLORS[index]}
+                  className="stroke-background hover:opacity-80"
+                />
+              ))}
+            </Pie>
+          </PieChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}

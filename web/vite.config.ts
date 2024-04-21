f7c9a479832fbe0f90d6b544e998caa1c2e624ca
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-21 10:29:02 -0300

    feat: add vitest config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,13 +1,18 @@
-import path from 'node:path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import type { UserConfig } from "vite";
+import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  test: {
+    globals: true,
+  },
+} as UserConfig & { test: InlineConfig });

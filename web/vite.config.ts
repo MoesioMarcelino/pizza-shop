f30109567e314fc85d1d8ceb3345bb37fb1218ed
MoesioMarcelino <moesiomarcelino1@gmail.com>
2024-04-21 10:42:48 -0300

    feat: setup happy-dom & testing-library/jest-dom

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -14,5 +14,7 @@ export default defineConfig({
   },
   test: {
     globals: true,
+    setupFiles: "./test/setup.ts",
+    environment: "happy-dom",
   },
 } as UserConfig & { test: InlineConfig });
